a862f75b7aa5f28f7fffdeeb44d023afb5fe773c
Eric Charles <eric@datalayer.io>
2018-01-02 08:33:55 +0100

    rename kuber to kuber-plane

--- incubator/deploy-charts.sh
+++ incubator/deploy-charts.sh
@@ -19,18 +19,18 @@
 
 function step_1() {
 
-  helm install -n heapster \
-    --namespace kube-system \
-    stable/heapster
+#  helm install -n heapster \
+#    --namespace kube-system \
+#    stable/heapster
 
-  helm install k8s-dashboard \
-    --namespace kube-system \
-    --set=httpPort=3000,resources.limits.cpu=200m,rbac.create=true \
-    -n k8s-dashboard
+#  helm install k8s-dashboard \
+#    --namespace kube-system \
+#    --set=httpPort=3000,resources.limits.cpu=200m,rbac.create=true \
+#    -n k8s-dashboard
 
-  helm install etcd \
-    --set StorageClass=gp2 \
-    -n kuber-etcd
+#  helm install etcd \
+#    --set StorageClass=gp2 \
+#    -n kuber-etcd
 
   helm install \
     --set persistence.nameNode.enabled=true \
@@ -64,7 +64,7 @@ spec:
     app: spitfire
 EOF
 
-  echo """
+echo """
 # Check HDFS
 
   kubectl exec -it hdfs-k8s-hdfs-k8s-hdfs-nn-0 -- hdfs dfsadmin -report
@@ -80,17 +80,23 @@ EOF
 # Browse Apache Zeppelin
 
   http://localhost:8001/api/v1/namespaces/default/services/http:spitfire-spitfire:8080/proxy
-
-# Before running the next step, check the LoadBalancer Ingress value for \`spitifire-lb\` (rerun in a few minutes if no hostname is shown)
-
-   kubectl describe services spitfire-lb | grep Ingress
 "
 
 }
 
 function step_2() {
 
-  kubectl describe services spitfire-lb | grep Ingress
+POD_NAMESPACE=default
+POD_NAME=$(kubectl get pods -n $POD_NAMESPACE -l app=nginx-ingress -o jsonpath={.items[0].metadata.name})
+kubectl describe pod $POD_NAME
+
+  echo "
+  
+# Before running the next step, check the LoadBalancer Ingress value for \`spitifire-lb\` (rerun in a few minutes if no hostname is shown)
+
+   kubectl describe services spitfire-lb | grep Ingress
+"
+  kubectl describe services spitfire-lb
   echo
   echo "Please enter the hostname created for the spitfire-lb:"
   read SPITFIRE_LB_HOSTNAME
@@ -101,34 +107,37 @@ function step_2() {
   echo
 
   helm install \
-    --set azure.applicationId="f145fb87-901d-4519-ad39-573c30770e6f" \
-    --set azure.redirect="http://$SPITFIRE_LB_HOSTNAME" \
+    --set azure.applicationId="f7194ac8-ff71-47f6-839c-e3b20f247ebc" \
+    --set azure.redirect="" \
     --set azure.scope="User.ReadBasic.All+Contacts.Read+Mail.Send+Files.ReadWrite+Notes.ReadWrite" \
     --set hdfs="" \
+    --set kuber.insecureBindAddress="0.0.0.0" \
     --set kuber.plane="" \
     --set kuber.rest="" \
     --set kuber.ws="" \
     --set spitfire.rest="http://$SPITFIRE_LB_HOSTNAME" \
     --set spitfire.ws="ws://$SPITFIRE_LB_HOSTNAME" \
     --set twitter.redirect="" \
-    --set kuber.insecureBindAddress="0.0.0.0" \
-    kuber \
-    -n kuber
+    kuber-plane \
+    -n kuber-plane
 
   cat << EOF | kubectl apply -f -
 apiVersion: v1
 kind: Service
 metadata:
-  name: kuber-lb
+  name: kuber-plane-web
   annotations:
-    service.beta.kubernetes.io/aws-load-balancer-backend-protocol: tcp
+    service.beta.kubernetes.io/aws-load-balancer-ssl-ports: 443
+    service.beta.kubernetes.io/aws-load-balancer-backend-protocol: ssl
+    service.beta.kubernetes.io/aws-load-balancer-connection-idle-timeout: 3600
+    service.beta.kubernetes.io/aws-load-balancer-ssl-cert: "arn:aws:acm:us-west-2:345579675507:certificate/9611ac0f-6d9c-4e2e-a4e4-0ec25f8d2156"
 spec:
   type: LoadBalancer
   ports:
-  - port: 80
+  - port: 443
     targetPort: 9091
   selector:
-    app: kuber
+    app: kuber-plane
 EOF
 
   echo "
@@ -143,13 +152,53 @@ EOF
 
 # Check the LoadBalancer Ingress value for \`kuber-lb\` (rerun in a few minutes if no hostname is shown)
 
-   kubectl describe services kuber-lb | grep Ingress
-
+   kubectl describe services kuber-https | grep Ingress
 "
-  kubectl describe services kuber-lb | grep Ingress
+  kubectl describe services kuber-https
   echo
 
 }
 
-# step_1
+function step_3() {
+  
+  cat << EOF | kubectl create -f -
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  annotations:
+    kubernetes.io/ingress.class: nginx
+  name: spitfire-ingress
+  namespace: default
+spec:
+  rules:
+  - http:
+      paths:
+      - path: /
+        backend:
+          serviceName: spitfire-spitfire
+          servicePort: 8080
+EOF
+
+  cat << EOF | kubectl create -f -
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  annotations:
+    kubernetes.io/ingress.class: nginx
+  name: kuber-ingress
+  namespace: default
+spec:
+  rules:
+  - http:
+      paths:
+      - path: /
+        backend:
+          serviceName: kuber-kuber
+          servicePort: 9091
+EOF
+
+}
+
+step_1
 step_2
+# step_3
